37afd4b5cb27149d83769debd70a8ca0c997e542
KrisztianRepei <reprisztian@gmail.com>
2026-02-06 14:50:17 +0100

    Update lfpLanguageMenu.js

--- components/lfpLanguageMenu.js
+++ components/lfpLanguageMenu.js
@@ -1,1 +1,14 @@
+import {
+  ActionRowBuilder,
+  StringSelectMenuBuilder
+} from "discord.js";
 
+export const lfpLanguageMenu = new ActionRowBuilder().addComponents(
+  new StringSelectMenuBuilder()
+    .setCustomId("lfp_language")
+    .setPlaceholder("Válassz nyelvet / Choose language")
+    .addOptions(
+      { label: "Magyar 🇭🇺", value: "hu" },
+      { label: "English 🇬🇧", value: "en" }
+    )
+);
