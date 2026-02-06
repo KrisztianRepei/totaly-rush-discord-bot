d7f582ed2bbacd101be3d96a990b2862420a0808
KrisztianRepei <reprisztian@gmail.com>
2026-02-06 14:50:00 +0100

    Update interactionHandler.js

--- handlers/interactionHandler.js
+++ handlers/interactionHandler.js
@@ -1,1 +1,25 @@
+import { lfpModal } from "../components/lfpModal.js";
+import { buildLfpMessage } from "../utils/lfpTemplates.js";
+
+export async function handleInteraction(interaction) {
+  if (interaction.isStringSelectMenu() && interaction.customId === "lfp_language") {
+    await interaction.showModal(lfpModal(interaction.values[0]));
+  }
+
+  if (interaction.isModalSubmit() && interaction.customId.startsWith("lfp_modal_")) {
+    const lang = interaction.customId.split("_")[2];
+
+    const data = {
+      room: interaction.fields.getTextInputValue("room"),
+      players: interaction.fields.getTextInputValue("players"),
+      rank: interaction.fields.getTextInputValue("rank"),
+      roles: interaction.fields.getTextInputValue("roles")
+    };
+
+    const message = buildLfpMessage(lang, data);
+
+    await interaction.channel.send(message);
+    await interaction.reply({ content: "Kész ✅", ephemeral: true });
+  }
+}
 
