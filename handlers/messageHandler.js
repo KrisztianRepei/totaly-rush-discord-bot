c1bed2c74df80a3c7d070b74fa7fae33b8a6a86c
KrisztianRepei <reprisztian@gmail.com>
2026-02-06 14:52:50 +0100

    Update messageHandler.js

--- handlers/messageHandler.js
+++ handlers/messageHandler.js
@@ -1,13 +1,20 @@
 import { lfpLanguageMenu } from "../components/lfpLanguageMenu.js";
 
+const ALLOWED_CHANNELS = [
+  "1467188892863168716",
+  "1467199549247328448"
+];
+
 export async function handleMessage(message) {
   if (message.author.bot) return;
+
+  // ❗ csak ezekben a szobákban
+  if (!ALLOWED_CHANNELS.includes(message.channel.id)) return;
+
   if (message.content.toLowerCase() !== "lfp") return;
 
   await message.reply({
     content: "Válaszd ki a nyelvet / Choose language 👇",
-    components: [lfpLanguageMenu],
-    ephemeral: true
+    components: [lfpLanguageMenu]
   });
 }
-
