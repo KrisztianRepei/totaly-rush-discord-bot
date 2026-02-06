1f1116ff35d1f6040ae442e57dfe1245088966b6
KrisztianRepei <reprisztian@gmail.com>
2026-02-06 14:50:27 +0100

    Update lfpModal.js

--- components/lfpModal.js
+++ components/lfpModal.js
@@ -1,1 +1,29 @@
+import {
+  ModalBuilder,
+  TextInputBuilder,
+  TextInputStyle,
+  ActionRowBuilder
+} from "discord.js";
+
+export function lfpModal(lang) {
+  return new ModalBuilder()
+    .setCustomId(`lfp_modal_${lang}`)
+    .setTitle(lang === "hu" ? "LFP adatok" : "LFP details")
+    .addComponents(
+      input("room", lang === "hu" ? "Szoba száma" : "Room number"),
+      input("players", lang === "hu" ? "Hány játékos?" : "Players needed"),
+      input("rank", lang === "hu" ? "Rang" : "Rank"),
+      input("roles", lang === "hu" ? "Pozíció(k)" : "Roles")
+    );
+}
+
+function input(id, label) {
+  return new ActionRowBuilder().addComponents(
+    new TextInputBuilder()
+      .setCustomId(id)
+      .setLabel(label)
+      .setStyle(TextInputStyle.Short)
+      .setRequired(true)
+  );
+}
 
