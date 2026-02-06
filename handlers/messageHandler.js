01eb1468a3c11fcd2c00587a3684f8e11e2ba7c4
KrisztianRepei <reprisztian@gmail.com>
2026-02-06 14:49:50 +0100

    Update messageHandler.js

--- handlers/messageHandler.js
+++ handlers/messageHandler.js
@@ -1,1 +1,13 @@
+import { lfpLanguageMenu } from "../components/lfpLanguageMenu.js";
+
+export async function handleMessage(message) {
+  if (message.author.bot) return;
+  if (message.content.toLowerCase() !== "lfp") return;
+
+  await message.reply({
+    content: "Válaszd ki a nyelvet / Choose language 👇",
+    components: [lfpLanguageMenu],
+    ephemeral: true
+  });
+}
 
