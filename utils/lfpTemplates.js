fb4b030ebf5bfa4908db4b34be0aaea69a20aea5
KrisztianRepei <reprisztian@gmail.com>
2026-02-08 14:30:24 +0100

    Update lfpTemplates.js

--- utils/lfpTemplates.js
+++ utils/lfpTemplates.js
@@ -1,12 +1,12 @@
 export function buildLfpMessage(lang, d, user) {
   if (lang === "hu") {
-    return `🔊${d.room} szobába keresünk ${d.players} playert.
+    return `🔊 | ${d.room} szobába keresünk ${d.players} playert.
 🏆 Rang/Pont: ${d.elo}
 🧠 Pozíció: ${d.role}
 👤 LFP: ${user}`;
   }
 
-  return `🔊 | Premier room #${d.room} looking for ${d.players} player.
+  return `🔊 | ${d.room} looking for ${d.players} player.
 🏆 Rank/Elo: ${d.elo}
 🧠: Role: ${d.role}
 👤 LFP: ${user}`;
